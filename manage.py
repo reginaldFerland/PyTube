2abc6fc41720118c73ac191c80a1e6b6374c4d48
Reginald L Ferland <reginald@ferland.io>
2018-05-20 20:03:42 +0000

    started database design

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 #! venv/bin/python3
 
-from PyTube import app
+from PyTube import app, db
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 def run(debug=False):
    app.run(host='0.0.0.0', debug=debug)
