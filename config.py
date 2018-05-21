aff3d3f51cf088f386cea95ac3fab3fc6852f502
Reginald L Ferland <reginald@ferland.io>
2018-05-21 03:34:28 +0000

    added config option, new test classes

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    ENV = 'development'
 
     # Database settings
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
--- tests/tests.py
+++ tests/tests.py
@@ -8,16 +8,20 @@ class BaseCase(TestCase):
     def create_app(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
         app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
         return app
 
     def setUp(self):
         db.create_all()
         self.client = app.test_client()
+        # Register data
+        self.registerForm = dict(username='tester', email='e@e.com')
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
+class HomePage(BaseCase):
     def test_home_page_loads(self):
         result = self.client.get('/') 
         self.assertEqual(result.status_code, 200) 
@@ -26,6 +30,7 @@ class BaseCase(TestCase):
         result = self.client.get('/')
         self.assert_template_used('index.html')
 
+class RegisterPage(BaseCase):
     def test_register_loads(self):
         result = self.client.get('/register')
         self.assertEqual(result.status_code, 200) 
@@ -34,6 +39,10 @@ class BaseCase(TestCase):
         result = self.client.get('/register')
         self.assert_template_used('register.html')
 
+    def test_register_create_account_code(self):
+        result = self.client.post('/register', data=self.registerForm, follow_redirects=True)
+        self.assertEqual(result.status_code, 302) #201 means creation but we redirect to login page instead 
+        
 #    def test_login(self):
 
 #    def test_logout(self):
