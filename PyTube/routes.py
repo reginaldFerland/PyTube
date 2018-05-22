1b67ebbe6116a04de81f7dbbedcf78a27d14ed1b
Reginald L Ferland <reginald@ferland.io>
2018-05-22 20:03:23 +0000

    user is now logged in

--- PyTube/routes.py
+++ PyTube/routes.py
@@ -2,6 +2,7 @@ from PyTube import app, db
 from flask import render_template, redirect, url_for, flash
 from PyTube.forms import RegistrationForm, LoginForm
 from PyTube.models import User
+from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/index')
@@ -24,5 +25,11 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        # User doesn't exist or password wrong, flash error
+        if user is None or not user.check_password(form.password.data):
+#            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,7 @@ from test_basecase import BaseCase
 #from PyTube import db
 from flask import url_for
 #from PyTube.models import User
+from flask_login import current_user
 
 class LoginPage(BaseCase):
     def test_login_loads(self):
@@ -16,4 +17,15 @@ class LoginPage(BaseCase):
     def test_login_redirects(self):
         result = self.client.post('/login', data=self.loginForm)
         self.assertRedirects(result, url_for('index')) 
+
+    def test_login_works(self):
+        with self.client:
+            result = self.client.post('/login', data=self.loginForm)
+            self.assertEquals(current_user.username, "user")
+
+    def test_login_rejects(self):
+        loginForm_wrong_password = dict(username='user', email='user@email.com', password='hunter2')
+        with self.client:
+            result = self.client.post('/login', data=loginForm_wrong_password)
+            self.assertTrue(current_user.is_anonymous)
 
