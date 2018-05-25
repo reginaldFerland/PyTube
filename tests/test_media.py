cbae51bb3d8ee723f55a808bc8d96a5a3d83786e
Reginald L Ferland <reginald@ferland.io>
2018-05-25 22:29:34 +0000

    Added controls to video

--- tests/test_media.py
+++ tests/test_media.py
@@ -65,4 +65,15 @@ class TestMedia(BaseCase):
         self.assertIn("<picture", str(result.data))
         self.assertIn("</picture>", str(result.data))
 
+    def test_media_video_controls(self):
+        filename = open("./tests/files/upload_video.mp4", mode='rb')
+        self.uploadForm = dict(name='video',media=filename)
+ 
+        with self.client:
+            self.client.post('/login', data=self.loginForm)
+            self.client.post('/upload', data=self.uploadForm)
+            
+        result = self.client.get('/media/1')
+        self.assertIn("controls", str(result.data))
+
 
