7321992ac987dd09afcd2f3b62053886957c4e6a
Reginald L Ferland <reginald@ferland.io>
2018-05-26 19:49:39 +0000

    added tests and fixed misnamed test

--- tests/test_user.py
+++ tests/test_user.py
@@ -29,7 +29,7 @@ class TestUser(BaseCase):
         self.assertTrue(bob.followed.filter(followers.c.followed_id == self.user.id).count() > 0)
         self.assertTrue(bob.followed.all()[0].username == "user")
 
-    def test_user_is_following(self):
+    def test_user_follow_duplicate(self):
         bob = User(username="bob", email="bob@bob.com")
         bob.set_password("hunter2")
         bob.save()
@@ -37,3 +37,13 @@ class TestUser(BaseCase):
         
         with self.assertRaises(Exception):
             bob.follow(self.user)
+
+    def test_user_is_following(self):
+        bob = User(username="bob", email="bob@bob.com")
+        bob.set_password("hunter2")
+        bob.save()
+
+        self.assertFalse(bob.is_following(self.user))
+        bob.follow(self.user)
+        self.assertTrue(bob.is_following(self.user))
+ 
