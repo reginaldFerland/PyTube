15b89bf7552af15addda2a94fe7dfcebed846f9d
Reginald L Ferland <reginald@ferland.io>
2018-05-30 16:38:07 +0000

    Refactored tests

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -6,65 +6,58 @@ from test_basecase import BaseCase
 import os.path
 
 class UploadPage(BaseCase):
+    def setUp(self):
+        super(UploadPage, self).setUp()
+        # Logged in client
+        with self.client as self.logged_in:
+            self.logged_in.post('/login', data=self.loginForm)
+ 
     def test_upload_page_loads(self):
-        with self.client:
-            self.client.post('/login', data=self.loginForm)
-            result = self.client.get('/upload')
+        result = self.client.get('/upload')
         self.assertEqual(result.status_code, 200) 
 
     def test_upload_page_template(self):
-        with self.client:
-            self.client.post('/login', data=self.loginForm)
-            result = self.client.get('/upload')
+        result = self.client.get('/upload')
         self.assert_template_used('upload.html')
 
     def test_upload_requires_login(self):
+        self.client.get('/logout')
         result = self.client.get('/upload')
         self.assertEqual(result.status_code, 302)
 
     def test_upload_redirects_to_media(self):
         filename = open("./tests/files/upload_file.txt")
         uploadForm = dict(name='picture',media=filename, public=True)
-        with self.client:
-            self.client.post('/login', data=self.loginForm)
-            result = self.client.post('/upload', data=uploadForm)
-            media = Media.query.filter_by(name=uploadForm['name']).first() 
-            self.assertRedirects(result, url_for('media', mediaID=media.id)) 
+        result = self.client.post('/upload', data=uploadForm)
+        media = Media.query.filter_by(name=uploadForm['name']).first() 
+        self.assertRedirects(result, url_for('media', mediaID=media.id)) 
 
     def test_upload_creates_media(self):
         filename = open("./tests/files/upload_file.txt")
         uploadForm = dict(name='text',media=filename, public=True)
-        with self.client:
-            self.client.post('/login', data=self.loginForm)
-            result = self.client.post('/upload', data=uploadForm)
-            media = Media.query.filter_by(name=uploadForm['name']).first() 
-            self.assertIsNotNone(media)
+        result = self.client.post('/upload', data=uploadForm)
+        media = Media.query.filter_by(name=uploadForm['name']).first() 
+        self.assertIsNotNone(media)
            
     def test_upload_uses_correct_name(self):
         filename = open("./tests/files/upload_file.txt")
         uploadForm = dict(name='text',media=filename, public=True)
-        with self.client:
-            self.client.post('/login', data=self.loginForm)
-            result = self.client.post('/upload', data=uploadForm)
-            media = Media.query.filter_by(name=uploadForm['name']).first() 
-            self.assertEquals(media.name, uploadForm['name'])
+        result = self.client.post('/upload', data=uploadForm)
+        media = Media.query.filter_by(name=uploadForm['name']).first() 
+        self.assertEquals(media.name, uploadForm['name'])
 
     def test_upload_creates_file(self):
         filename = open("./tests/files/upload_file.txt")
         uploadForm = dict(name='text',media=filename, public=True)
-        with self.client:
-            self.client.post('/login', data=self.loginForm)
-            result = self.client.post('/upload', data=uploadForm)
-            media = Media.query.filter_by(name=uploadForm['name']).first() 
+        result = self.client.post('/upload', data=uploadForm)
+        media = Media.query.filter_by(name=uploadForm['name']).first() 
         self.assertTrue(os.path.exists(media.path))
         self.assertTrue(os.path.isfile(media.path))
 
     def test_upload_flash_message(self):
         filename = open("./tests/files/upload_file.txt")
         uploadForm = dict(name='text',media=filename, public=True)
-        with self.client:
-            self.client.post('/login', data=self.loginForm)
-            result = self.client.post('/upload', data=uploadForm)
+        result = self.client.post('/upload', data=uploadForm)
             
         with self.client.session_transaction() as sess:
             flash_message = dict(sess['_flashes'])
