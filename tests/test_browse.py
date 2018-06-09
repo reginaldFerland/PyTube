da3108d98e55cff02a8eea5cdee7e092aff79241
Reginald L Ferland <reginald@ferland.io>
2018-06-09 16:50:56 +0000

    Added tests for get_most_recent

--- tests/test_browse.py
+++ tests/test_browse.py
@@ -62,7 +62,6 @@ class TestHome(BaseCase):
         self.assertEquals(result, test)
 
     def test_browse_limit(self):
-        self.upload_jpg['public'] = False
         self.logged_in.post('/upload', data=self.upload_jpg)
         media1 = Media.query.filter_by(id=1).first()
         self.logged_in.post('/upload', data=self.upload_mp4)
@@ -71,7 +70,28 @@ class TestHome(BaseCase):
         result = browse(user=self.user, limit=1)
         self.assertEquals(len(result), 1)
 
-    def test_get_most_recent(self):
+    def test_get_most_recent_default(self):
+        self.logged_in.post('/upload', data=self.upload_jpg)
+        media1 = Media.query.filter_by(id=1).first()
+        self.logged_in.post('/upload', data=self.upload_mp4)
+        media2 = Media.query.filter_by(id=2).first()
+        test = [media2 , media1]
+
+        result = get_most_recent()
+        self.assertEquals(result, test)
+
+    def test_get_most_recent_private(self):
+        self.upload_jpg['public'] = False
+        self.logged_in.post('/upload', data=self.upload_jpg)
+        media1 = Media.query.filter_by(id=1).first()
+        self.logged_in.post('/upload', data=self.upload_mp4)
+        media2 = Media.query.filter_by(id=2).first()
+        test = [media2]
+
+        result = get_most_recent()
+        self.assertEquals(result, test)
+
+    def test_get_most_recent_user(self):
         self.upload_jpg['public'] = False
         self.logged_in.post('/upload', data=self.upload_jpg)
         media1 = Media.query.filter_by(id=1).first()
@@ -82,5 +102,12 @@ class TestHome(BaseCase):
         result = get_most_recent(user=self.user)
         self.assertEquals(result, test)
 
+    def test_browse_limit(self):
+        self.logged_in.post('/upload', data=self.upload_jpg)
+        media1 = Media.query.filter_by(id=1).first()
+        self.logged_in.post('/upload', data=self.upload_mp4)
+        media2 = Media.query.filter_by(id=2).first()
 
-    #def test_get_most_recent_limit(self):
+        result = browse(user=self.user, limit=1)
+        self.assertEquals(len(result), 1)
+
